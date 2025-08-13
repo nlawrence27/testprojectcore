46795a61699f939aca19abfcc7ac5c275ff2d14d
MAONLY2410 <bymynimonya@gmail.com>
2025-08-13 14:35:49 -0600

    Prueba 13/8/2025, 10

---
+++ Migrations/20250813203539_InitialCustomerAddress.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestProjectCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCustomerAddress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "address",
+                table: "Customers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "address",
+                table: "Customers");
+        }
+    }
+}
--- obj/Debug/net9.0/TestProjectCore.AssemblyInfo.cs
+++ obj/Debug/net9.0/TestProjectCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProjectCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34a11af5c180dc8841fd223c1aa9b020bc91db6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19dc729a9e4372cf0a9f07fc5b0cc3328308de57")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProjectCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProjectCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
