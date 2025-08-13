eccc7a140101bde7f1296447e651371a1c3b0128
MAONLY2410 <bymynimonya@gmail.com>
2025-08-13 14:33:03 -0600

    Prueba 13/8/2025, 9

--- Models.cs
+++ Models.cs
@@ -8,4 +8,5 @@ public class Customer
     public string first_name { get; set; }
     public string last_name { get; set; }
     public int phone { get; set; }
+    public string address { get; set; }
 }
